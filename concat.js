2e7196347545618f8a5422812895290fcdb4acf5
dejair8005 <68168713+dejair8005@users.noreply.github.com>
2023-06-22 14:40:55 -0300

    Concat and FlatMap

---
+++ concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Ualeska', 'Cibalena']
+const filhos = ['Mateus', 'Diogo']
+const todos = filhas.concat(filhos, 'fulano')
+console.log(todos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[5, 6]]))
